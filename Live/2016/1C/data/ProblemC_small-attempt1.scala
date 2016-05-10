72d882b43012cb2ab2a721600ebeff0a95329201
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2016-05-09 20:38:45 -0400

    live 2016.1C.C: solution to problem C
    - solved small and large problems in the final 2 minutes
    - wasn't planning to do round 1C, but woke at 5:30, got up at 6:00,
      logged in an hour after the round started, saw that the 1000th
      position hadn't got very far yet, had a look at the hard problem,
      and decided it would be fun to see how well I could do
      with all pressure of trying to get through removed
    - surprisingly I was only 5 points short of reaching the top 1000
    - Would I have got through if I had started at 5:00?
      Maybe. Or maybe it was being on the back foot that motivated me.

---
+++ Live/2016/1C/data/ProblemC_small-attempt1.scala
@@ -0,0 +1,54 @@
+import scala.io._
+import java.io._
+import java.util.Scanner
+import scala.collection._
+import scala.collection.immutable.List
+
+object ProblemC {
+  def main(args: Array[String]): Unit = {
+    processFiles(args(0), args(1))
+  }
+
+  def processFiles(inputFilePath: String, outputFilePath: String): Unit = {
+    val bufferedSource = Source.fromFile(inputFilePath)
+    try {
+      val lines = bufferedSource.getLines()
+      val outputFile = new File(outputFilePath)
+      val bw = new BufferedWriter(new FileWriter(outputFile))
+      try {
+        val testCaseCount = lines.next().toInt
+        for (testCase <- 1 to testCaseCount) {
+          val line = lines.next()
+          val scanner = new Scanner(line)
+          val j = scanner.nextInt()
+          val p = scanner.nextInt()
+          val s = scanner.nextInt()
+          val k = scanner.nextInt()
+          val solution = solve(j, p, s, k)
+          val dayCount = solution.length
+          bw.write(s"Case #$testCase: $dayCount")
+          bw.newLine()
+          for ((jix, kix, skix) <- solution) {
+            bw.write(s"$jix $kix $skix")
+            bw.newLine()
+          }
+        }
+      } finally {
+        bw.flush()
+        bw.close()
+      }
+    } finally {
+      bufferedSource.close()
+    }
+  }
+
+  def solve(j: Int, p: Int, s: Int, k: Int): IndexedSeq[Tuple3[Int, Int, Int]] = {
+    val sk = math.min(s, k)
+    val combos = for {
+      jix <- 1 to j
+      pix <- 1 to p
+      skix <- 1 to sk
+    } yield (jix, pix, (jix + pix + skix - 1) % s + 1)
+    combos
+  }
+}
---
+++ Live/2016/1C/src/main/scala/Master.scala
@@ -0,0 +1,66 @@
+import java.io.File
+import java.nio.file.{Files, Paths, StandardCopyOption}
+
+import scala.collection.immutable
+
+/**
+  * Created by Andrew on 2016/04/15.
+  */
+object Master {
+  val sizeMap = immutable.HashMap[String, String]("s" -> "small", "l" -> "large", "t"->"test")
+
+  // e.g. D s 0
+  //      D t
+  //      D l data
+  def main(args: Array[String]): Unit = {
+    try {
+      if (args.length < 2) {
+        println("USAGE: problem size [attempt [inputFolder [outputFolder]]]")
+        println()
+        println("problem: a|b|...")
+        println("size: s|small|l|large|t|test|{custom}")
+        println("attempt: 0, 1, ... (only relevant for size=small, defaults to 0)")
+        println("inputFolder: defaults to data, NB: no trailing slash")
+        println("outputFolder: defaults to match inputFolder, NB: no trailing slash")
+      } else {
+        val problem = args(0).toUpperCase()
+        val sizeCode = args(1).toLowerCase()
+        val sizeName = sizeMap.getOrElse(sizeCode, sizeCode)
+        val isSmall = sizeName == "small"
+        val inFolderPos = if (isSmall) 3 else 2
+        val attempt = if (isSmall && args.length > 2) args(2).toInt else 0
+        val inFolderPath = if (args.length > inFolderPos) args(inFolderPos) else "data"
+        val outFolderPath = if (args.length > inFolderPos + 1) args(inFolderPos) else inFolderPath
+        val attemptSuffix = if (isSmall) s"-attempt$attempt" else ""
+        val inFileName = s"$inFolderPath/$problem-$sizeName$attemptSuffix.in"
+        val outFileName = s"$outFolderPath/$problem-$sizeName$attemptSuffix.out"
+        run(problem, inFileName, outFileName)
+
+        // Copy source file for easy upload (only relevant if part of live competition):
+        if (sizeName == "small" || sizeName == "large") {
+          val srcFilePath = Paths.get(s"src/main/scala/Problem$problem.scala")
+          val destSrcFilePath = Paths.get(s"$outFolderPath/Problem${problem}_$sizeName$attemptSuffix.scala")
+          if (Files.exists(srcFilePath)) {
+            Files.copy(srcFilePath, destSrcFilePath, StandardCopyOption.REPLACE_EXISTING)
+          }
+        }
+      }
+    }
+    catch {
+      case e: Throwable => println(e)
+    }
+
+    def run(problem: String, inFileName: String, outFileName: String): Unit = {
+      if (!new File(inFileName).exists()) {
+        throw new IllegalArgumentException(s"ERROR: input file does not exist: $inFileName")
+      } else {
+        problem match {
+          // case "A"  => ProblemA.processFiles(inFileName, outFileName)
+          // case "B" => ProblemB.processFiles(inFileName, outFileName)
+          case "C" => ProblemC.processFiles(inFileName, outFileName)
+          case _ => throw new IllegalArgumentException(s"Unsupported problem: $problem")
+        }
+      }
+    }
+  }
+}
